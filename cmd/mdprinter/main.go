709f40d6b66807f56dce80ac839a1d4cd29575c4
Aniki <anikipelau@gmail.com>
2024-02-23 13:12:37 +0300

    add version printing

--- cmd/mdprinter/main.go
+++ cmd/mdprinter/main.go
@@ -10,17 +10,25 @@ import (
 	"github.com/trueaniki/mdprinter"
 )
 
+var version = "0.0.0"
+
 var opts struct {
-	Style  string `short:"s" long:"style" description:"style of the pdf, available styles are: modest, retro, air, splendor" default:"modest"`
-	Align  string `short:"a" long:"align" description:"align of the pdf content, available aligns are: left, center, right" default:"left"`
-	Custom string `short:"c" long:"custom" description:"custom css file path, if provided, it will override the style and align options" default:"none"`
-	Data   string `short:"d" long:"data" description:"JSON data file path, if provided, it will interpolate the data into the markdown file" default:"none"`
+	Style   string `short:"s" long:"style" description:"style of the pdf, available styles are: modest, retro, air, splendor" default:"modest"`
+	Align   string `short:"a" long:"align" description:"align of the pdf content, available aligns are: left, center, right" default:"left"`
+	Custom  string `short:"c" long:"custom" description:"custom css file path, if provided, it will override the style and align options" default:"none"`
+	Data    string `short:"d" long:"data" description:"JSON data file path, if provided, it will interpolate the data into the markdown file" default:"none"`
+	Output  string `short:"o" long:"output" description:"output pdf file path" default:"none"`
+	Version bool   `short:"v" long:"version" description:"print version"`
 }
 
 func main() {
 	args, err := flags.ParseArgs(&opts, os.Args)
 	if err != nil {
 		printAndExit(err)
+	}
+	if opts.Version {
+		fmt.Println(version)
+		os.Exit(0)
 	}
 
 	mdPath := args[1]
@@ -30,6 +38,9 @@ func main() {
 		printAndExit(err)
 	}
 	pdfPath := getPDFPath(mdPath)
+	if opts.Output != "none" {
+		pdfPath = opts.Output
+	}
 
 	html := mdprinter.Parse(md)
 
