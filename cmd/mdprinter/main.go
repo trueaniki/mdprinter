b589f45748067186dff2201012737f071904aa96
Aniki <anikipelau@gmail.com>
2024-02-26 16:17:24 +0300

    update version

--- cmd/mdprinter/main.go
+++ cmd/mdprinter/main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/trueaniki/mdprinter"
 )
 
-var version = "0.1.0"
+var version = "0.2.0"
 
 var opts struct {
 	Style       string      `short:"s" long:"style" description:"style of the pdf, available styles are: modest, retro, air, splendor" default:"modest"`
