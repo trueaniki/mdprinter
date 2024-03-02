76cf73037736b84b87dc706bb20b15ef4d143f22
Aniki <anikipelau@gmail.com>
2024-03-02 15:01:44 +0300

    add process function

---
+++ process.go
@@ -0,0 +1,47 @@
+package mdprinter
+
+type Processor struct {
+	jsonData  interface{}
+	style     string
+	align     string
+	customCss string
+}
+
+func New() *Processor {
+	return &Processor{
+		jsonData:  nil,
+		style:     "air",
+		align:     "left",
+		customCss: "none",
+	}
+}
+
+func (p *Processor) Process(md []byte) ([]byte, error) {
+	html := Parse(md)
+	if p.jsonData != nil {
+		html = Interpolate(html, p.jsonData)
+	}
+	css := FormupCss(p.style, p.align, p.customCss)
+	html = append(html, []byte("<style>"+css+"</style>")...)
+	return Print(html)
+}
+
+func (p *Processor) WithInterpolation(jsonData interface{}) *Processor {
+	p.jsonData = jsonData
+	return p
+}
+
+func (p *Processor) WithStyle(style string) *Processor {
+	p.style = style
+	return p
+}
+
+func (p *Processor) WithAlign(align string) *Processor {
+	p.align = align
+	return p
+}
+
+func (p *Processor) WithCustomCss(css string) *Processor {
+	p.customCss = css
+	return p
+}
